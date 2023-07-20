2fdb98e28c16a62319992bf94ee5cdc477d27f72
keeliiia <s10258719@connect.np.edu.sg>
2023-07-20 23:22:25 +0800

    IM TRYING BUT IDK WHAT IM DOING TBH, i will continue tmr

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,2 +1,15 @@
-print("keelia")
-print("helloooooooo")
+from pathlib import Path 
+import csv 
+
+fp = Path("C:\IGP_PFB_git\project_group2_KESI\csv_reports\cash-onhand.csv")
+with fp.open(mode="r", encoding="latin-1", newline="") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    cashOnHand = []
+
+    for row in reader: 
+        cashOnHand.append ([row[0],row[1]])
+
+def cash_on_hand():
+    for 
