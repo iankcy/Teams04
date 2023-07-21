378b75965c6827132c257b52c59d0b5662cdb3f2
“Nursuadah” <S10262179@connect.np.edu.sg>
2023-07-21 17:58:01 +0800

    for now

--- profit_loss.py
+++ profit_loss.py
@@ -1,2 +1,31 @@
-print("shuadah")
-print("this is mafan")
+from pathlib import Path 
+import csv 
+
+#create a path for "profit_and_loss.csv" 
+fp = Path("/Users/nurshuadahnoor/git/project_group2_KESI/csv_reports/profit_and_loss.csv") 
+with fp.open(mode="r", encoding="latin-1", newline="") as file:
+#create a "csv reader" object
+     reader= csv.reader(file)
+
+def compute_net_profit_difference(net_profit_column):
+    differences = []
+    highest_increment_day = None
+    highest_increment_amount = 0
+
+    for i in range(1, len(net_profit_column)):
+        #calculate the difference in net profit between the current day and the previous day
+        difference = net_profit_column[i] - net_profit_column[i - 1]
+        differences.append(difference)
+
+        # Check if the current difference is higher than the highest increment so far
+        if difference > highest_increment_amount:
+            highest_increment_amount = difference
+            highest_increment_day = i
+    
+    print("Net Profit Differences:")
+    for day, diff in enumerate(differences, 1):
+        print(f"Day {day}: Net profit difference - ${diff}")
+
+    print(f"\nHighest increment occurred on Day {highest_increment_day + 1}, "
+          f"with an amount of ${highest_increment_amount}")
+    
