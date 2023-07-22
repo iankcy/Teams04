6e8e1a6999c1e823bdd8f496c34100cbc6c98f73
“Nursuadah” <S10262179@connect.np.edu.sg>
2023-07-22 14:28:23 +0800

    update

--- profit_loss.py
+++ profit_loss.py
@@ -21,11 +21,31 @@ def compute_net_profit_difference(net_profit_column):
         if difference > highest_increment_amount:
             highest_increment_amount = difference
             highest_increment_day = i
-    
+
+ #print the net profit difference   
     print("Net Profit Differences:")
     for day, diff in enumerate(differences, 1):
         print(f"Day {day}: Net profit difference - ${diff}")
 
+#print the highest increment occured on the days
     print(f"\nHighest increment occurred on Day {highest_increment_day + 1}, "
           f"with an amount of ${highest_increment_amount}")
     
+#
+    surplus_count = 0
+    for i in range(1, len(net_profit_column)):
+        if net_profit_column[i] > net_profit_column[i - 1]:
+            surplus_count += 1
+
+    with open("summary_report.txt", "w") as summary_file:
+        summary_file.write("Net Profit Differences:\n")
+        for day, diff in enumerate(differences, 1):
+            summary_file.write(f"Day {day}: Net profit difference - ${diff}\n")
+
+        summary_file.write(f"\nHighest increment occurred on Day {highest_increment_day + 1}, "
+                           f"with an amount of ${highest_increment_amount}\n")
+
+        if surplus_count == len(net_profit_column) - 1:
+            summary_file.write("\n[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN PREVIOUS DAY\n")
+        else:
+            summary_file.write("\n[NET PROFIT SURPLUS] NET PROFIT DOES NOT INCREASE ON EVERY DAY\n")
