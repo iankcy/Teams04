6a43f3a981ec6be4f68534c104e092796a3776ad
Eilleencyl <eilleenchua1203@gmail.com>
2023-08-09 22:20:34 +0800

    docstrings

--- overheads.py
+++ overheads.py
@@ -2,20 +2,27 @@ import csv
  
  
 def find_highest_overhead(fp): 
+    '''
+    This function is to help find the highest overhead and print out the category as well as the value in percentage. 
+    '''
     highest_overhead = 0 
     highest_category = "" 
+    
+    # To calculate the total overhead percentage 
+    total_overheads = 0  
  
-    total_overheads = 0  # To calculate the total overhead percentage 
- 
+    # Open the CSV file for reading
     with open(fp, mode="r", newline="") as file: 
         reader = csv.reader(file) 
-        next(reader) 
-        next(reader) 
- 
+        next(reader) # Skip the header
+        next(reader) # Skip the second line
+
+        # Iterate through each line in the CSV file
         for category, overhead in reader: 
             overhead = float(overhead) 
-            total_overheads += overhead 
- 
+            total_overheads += overhead
+
+            # Check if the current overhead is higher than the previously recorded highest overhead
             if overhead > highest_overhead: 
                 highest_overhead = overhead 
                 highest_category = category 
@@ -23,5 +30,5 @@ def find_highest_overhead(fp):
     # Calculate the percentage of the highest overhead category out of the total overheads 
     highest_overhead_percentage = (highest_overhead / total_overheads) * 100 
  
-    # return the output 
+    # Return the output 
     return f"[HIGHEST OVERHEAD] {highest_category}: {round(highest_overhead_percentage,2)}%"
